fcf25df4707999b1920f3f2d8244dc518629f97b
Changchen He <yhfy2006@gmail.com>
2016-09-05 22:20:16 -0400

    added animation.

--- QProject/LoggingViewController.swift
+++ QProject/LoggingViewController.swift
@@ -12,8 +12,13 @@ import UIColor_Hex_Swift
 class ViewController: UIViewController {
 
     let centerRoundShape = CAShapeLayer()
-    let statusPointShape = CAShapeLayer()
-    let startAnimation = CAKeyframeAnimation(keyPath: "position")
+    var statusPointView = UIView()
+    var startAnimation = CAKeyframeAnimation(keyPath: "position")
+    var animating = false
+    
+    let ringBlue = Util.hexStringToUIColor("25BFF0").CGColor
+    let buttonGrey = Util.hexStringToUIColor("979797").CGColor
+    
     @IBOutlet var launchButton:UIButton?
     
     override func viewDidLoad() {
@@ -38,7 +43,7 @@ class ViewController: UIViewController {
         let radis = launchButton!.bounds.size.width/2.0 + 10
         centerRoundShape.path = Util.makeCircleAtLocation(view.center, radius: radis).CGPath
         
-        centerRoundShape.strokeColor = Util.hexStringToUIColor("25BFF0").CGColor
+        centerRoundShape.strokeColor = self.buttonGrey
         centerRoundShape.fillColor = nil
         centerRoundShape.lineWidth = 3.0
         //centerRoundShape.bounds = CGPathGetPathBoundingBox(centerRoundShape.path)
@@ -46,57 +51,48 @@ class ViewController: UIViewController {
         
         // start building animation path
         let startingPoint = CGPointMake(self.launchButton!.center.x, self.launchButton!.center.y - radis)
-        self.statusPointShape.path = Util.makeCircleAtLocation(startingPoint, radius: 7).CGPath
-        self.statusPointShape.strokeColor = Util.hexStringToUIColor("25BFF0").CGColor
-        self.statusPointShape.fillColor = self.view.backgroundColor?.CGColor
-        self.statusPointShape.lineWidth = 3.0
-        
-            
-        view.layer.addSublayer(statusPointShape)
-        
-        
-//        let anchorPoint = self.launchButton!.center
-//        self.statusPointShape.anchorPoint = anchorPoint
-//        let rotateAnimation = CABasicAnimation(keyPath:"position")
-//        rotateAnimation.toValue =  CGFloat(M_PI)*2 // The angle we are rotating to
-//        self.statusPointShape.addAnimation(rotateAnimation, forKey: "position")
-        
-        let boundingRect = CGRectMake(self.launchButton!.frame.origin.x, self.launchButton!.frame.origin.y, self.launchButton!.frame.size.width, self.launchButton!.frame.size.height)
         let orbit = CAKeyframeAnimation(keyPath:"position")
-        orbit.duration = 3
-        
-        let testShape = CAShapeLayer()
-        testShape.path = CGPathCreateWithEllipseInRect(CGPathGetPathBoundingBox(centerRoundShape.path),nil)
-        testShape.fillColor = UIColor.redColor().CGColor
-        testShape.strokeColor = UIColor.greenColor().CGColor
-        view.layer.addSublayer(testShape)
-        
-        
+        orbit.duration = 5
+    
         orbit.path = CGPathCreateWithEllipseInRect(CGPathGetPathBoundingBox(centerRoundShape.path),nil)
         orbit.calculationMode = kCAAnimationPaced
         orbit.repeatCount = Float.infinity
         orbit.rotationMode = kCAAnimationRotateAuto;
-        self.statusPointShape.addAnimation(orbit, forKey: "Move7")
+        
+        self.startAnimation = orbit
         
         
-        let aView = UIView(frame: CGRectMake(0, 0, 50, 50))
-        aView.backgroundColor = UIColor.grayColor()
-        self.view.addSubview(aView)
+        let aView = UIView(frame: CGRectMake(0, 0, 14, 14))
+        aView.layer.cornerRadius = aView.frame.size.width/2
+        aView.backgroundColor = self.view.backgroundColor
+        aView.clipsToBounds = true
+        aView.layer.borderColor = self.ringBlue
+        aView.layer.borderWidth = 2.0
         
-        
-        
-//        startAnimation.duration = 3
-//        startAnimation.repeatCount = Float.infinity
-//        startAnimation.path = Util.makeCircleAtLocation(self.launchButton!.center, radius: radis).CGPath
-//        startAnimation.calculationMode = kCAAnimationPaced
-//        startAnimation.fillMode = kCAFillModeForwards
-//        startAnimation.removedOnCompletion = false
-//        startAnimation.values = [0,CGFloat(M_PI),CGFloat(M_PI)*1.7,CGFloat(M_PI)*2]
-//        
-//        self.statusPointShape.addAnimation(startAnimation, forKey: "position")
+        self.statusPointView = aView
+        self.statusPointView.center = startingPoint
+        self.view.addSubview(self.statusPointView)
+        self.statusPointView.hidden = true
         
     }
     
+    @IBAction func launchButtonClicked(){
+        if(self.animating)
+        {
+            self.statusPointView.layer.removeAllAnimations()
+            self.animating = false
+            self.centerRoundShape.strokeColor = self.buttonGrey
+            self.statusPointView.hidden = true
+        }else
+        {
+            self.statusPointView.layer.addAnimation(self.startAnimation, forKey: "Move7")
+            self.animating = true
+            self.centerRoundShape.strokeColor = self.ringBlue
+            self.statusPointView.hidden = false
+        }
+    
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
